d21e6098d298fa7d8f6617f374f9adbfefaa58f4
Ahmed Sameh <samehgit@el-abda3.net>
2023-02-27 18:21:36 +0200

    15

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const http = require("http");
-const routes = require("./routes");
+const express = require("express");
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin.routes");
+const frontRoutes = require("./routes/front.routes");
 
-const SERVER = http.createServer(routes.handler);
+const app = express();
 
-SERVER.listen(8000);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(frontRoutes);
+
+app.use((req, res) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
+
+app.listen(3000);
