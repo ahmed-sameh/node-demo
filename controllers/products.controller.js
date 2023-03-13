260da9ce7278475bbc62e1e12ea80c2d0473b802
Ahmed Sameh <samehgit@el-abda3.net>
2023-03-13 19:01:14 +0200

    t

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require("../models/product.model");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -8,14 +8,17 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getAllProducts = (req, res, next) => {
-  res.render("shop", {
-    products: products,
-    path: "/",
-    pageTitle: "Shop",
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      products: products,
+      path: "/",
+      pageTitle: "Shop",
+    });
   });
 };
