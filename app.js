86a8c472efb7ccfa9c6a1224cbcf75e0e2cee47f
Ahmed Sameh <samehgit@el-abda3.net>
2023-04-26 12:24:43 +0200

    x

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const http = require("http");
-const routes = require("./routes");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const server = http.createServer(routes.routesHandler);
+const app = express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/add_product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use((req, res, next) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.listen(3000);
